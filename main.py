51acf5d819cd68c312ce6a1508edb389f3b42723
marmolad <48159338+marmolad@users.noreply.github.com>
2022-03-08 12:02:29 +0100

    1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=800, height=800)
@@ -9,6 +10,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 
@@ -16,10 +18,15 @@ game_on = True
 
 while game_on:
     screen.update()
-    screen.onkey(l_paddle.up, "w")
-    screen.onkey(l_paddle.down, "s")
-    screen.onkey(r_paddle.up, "i")
-    screen.onkey(r_paddle.down, "k")
+    screen.onkeypress(l_paddle.up, "w")
+    screen.onkeypress(l_paddle.down, "s")
+    screen.onkeypress(r_paddle.up, "i")
+    screen.onkeypress(r_paddle.down, "k")
+    screen.update()
+    ball.move()
+    if r_paddle.ycor() + 50 >= ball.ycor() >= r_paddle.ycor() - 50 and ball.xcor() >= r_paddle.xcor() - 20\
+            or l_paddle.ycor() + 50 >= ball.ycor() >= l_paddle.ycor() - 50 and ball.xcor() <= l_paddle.xcor() + 20:
+        ball.x_speed *= -1
 
 
 screen.exitonclick()
